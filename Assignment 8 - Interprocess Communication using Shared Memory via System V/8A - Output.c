42ada6ffa614ec2a57d5e75b80625fb2816ec8f9
Saarth Deshpande <deshpandesaarth@gmail.com>
2021-05-06 17:03:33 +0530

    SHM functions

--- Assignment 8 - Interprocess Communication using Shared Memory via System V/8A - Output.c
+++ Assignment 8 - Interprocess Communication using Shared Memory via System V/8A - Output.c
@@ -1,55 +1,62 @@
-#include<stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <errno.h>
-#include <unistd.h>
-#include <sys/msg.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
+#define TEXT_SZ 2048
+// Alternatively, BUFSIZ can also be used
 
-struct my_msg_st {
-	long int my_msg_type;
-	char some_text[BUFSIZ];
+// BUFSIZ value depends on preprocessor (below code from stdio.h)
+/*
+  #if defined(_M_MPPC)
+  #define BUFSIZ 4096
+  #else \/* defined (_M_MPPC) *\/
+  #define BUFSIZ 512
+  #endif \/* defined (_M_MPPC) *\/
+ */
+
+struct shared_use_st {
+    int written_by_you;
+    char some_text[TEXT_SZ];
 };
 
 int main()
 {
-	int running = 1;
-	int msgid;
-	struct my_msg_st some_data;
-	long int msg_to_receive = 0;
-	
-	msgid = msgget((key_t)1234, 0666 | IPC_CREAT);
+    int running = 1;
+    void *shared_memory = (void *)0;
+    struct shared_use_st *shared_stuff;
+    char buffer[BUFSIZ];
+    int shmid;
+    // Allocate shared memory
+    shmid = shmget((key_t)1234, sizeof(struct shared_use_st), 0666 | IPC_CREAT);
+    // IPC_CREAT has the value of 512 in decimal (depends on system)
+    // 0666 sets the access permissions of the memory segment
+    // IPC_CREAT tells the system to create a new memory segment for the shared memory
+    // Give program access to shared memory
+    shared_memory = shmat(shmid, (void *)0, 0);
+    printf("Memory attached at %X\n", (int)shared_memory);
 
-	// IPC_CREAT has the value of 512 in decimal (depends on system)
-	// 0666 sets the access permissions of the memory segment
-	// IPC_CREAT tells the system to create a new memory segment for the shared memory
-
-	if (msgid == -1) {
-		fprintf(stderr, "msgget failed with error: %d\n", errno);
-		exit(EXIT_FAILURE);
-	}
-	while(running) {
-		if (msgrcv(msgid, (void *)&some_data, BUFSIZ, msg_to_receive, 0) == -1) {   // if messages are not retrieved from queue
-			fprintf(stderr, "msgrcv failed with error: %d\n", errno);
-			exit(EXIT_FAILURE);
-		}
-		// BUFSIZ value depends on preprocessor (below code from stdio.h)
-		/*
-		  #if defined(_M_MPPC)
-		  #define BUFSIZ 4096
-		  #else \/* defined (_M_MPPC) *\/
-		  #define BUFSIZ 512
-          #endif \/* defined (_M_MPPC) *\/
-		 */
-
-		printf("You wrote: %s", some_data.some_text);
-		if (strncmp(some_data.some_text, "end", 3) == 0)    // compare at most first n(3) bytes of string 1 and 2
-			running = 0;
-	}
-
-	if (msgctl(msgid, IPC_RMID, 0) == -1)  {    // if failed to destroy message queue
-		fprintf(stderr, "msgctl(IPC_RMID) failed\n");
-		exit(EXIT_FAILURE);
-	}
-	exit(EXIT_SUCCESS);
+    shared_stuff = (struct shared_use_st *)shared_memory;
+    while(running) {
+        while(shared_stuff -> written_by_you == 1) {
+        // sleep(1);
+        // printf("waiting for server...\n");
+            continue;
+        }
+        printf("Enter some text: ");
+        fgets(buffer, BUFSIZ, stdin);
+        strncpy(shared_stuff -> some_text, buffer, TEXT_SZ);
+        shared_stuff -> written_by_you = 1;
+        if (strncmp(buffer, "end", 3) == 0) {
+            running = 0;
+        }
+    }
+    // Detach shared memory
+    shmdt(shared_memory);
+    // Delete Shared memory
+    shmctl(shmid, IPC_RMID, 0);
+    return 0;
 }
